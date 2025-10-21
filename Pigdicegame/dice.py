96dcd030ebae1cceef7a71c3acf179a304cd513a
JohnssonJoel <joel.johnsson@hotmail.se>
2025-10-21 17:09:05 +0200

    Added docstrings to dice.py and TestDice.py

--- Pigdicegame/dice.py
+++ Pigdicegame/dice.py
@@ -2,13 +2,12 @@ import random
 
 
 class Dice:
-    """Six sided dice."""
-
+    """Creates a six sided dice."""    
     def __init__(self, sides=6):
         if sides < 2:
             raise ValueError("A dice must have at least 2 sides.")
         self.sides = sides
 
+    """Return a random number between 1 and 6."""
     def roll(self):
-        """Return a random number between 1 and 6"""
         return random.randint(1, self.sides)
