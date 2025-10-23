f1b46499bfd177bec77d2d9bb6e82ef94294c0cc
MarcusPersson97 <marcus.persson.97@hotmail.com>
2025-10-23 11:27:30 +0200

    fixed docstrings

--- Pigdicegame/TestPlayer.py
+++ Pigdicegame/TestPlayer.py
@@ -3,20 +3,19 @@ from player import Player
 
 
 class TestPlayer(unittest.TestCase):
-    """
-    Unit tests for the player class.
-
+    
+    """Unit tests for the player class.
     This test suite verifies the correct behavior of player methods
     including setting and getting names and scores, as well as handling
     invalid input types.
     """
 
     def setUp(self):
-        """ Set up a new player instance before each test."""
+        """Set up a new player instance before each test."""
         self.p1 = Player("Marcus")
 
     def test_set_score(self):
-        """ Test setting a valid score and handling invalid score input. """
+        """Test setting a valid score and handling invalid score input."""
         self.p1.set_score(0)
         self.assertEqual(self.p1.score, 0)
 
@@ -24,7 +23,7 @@ class TestPlayer(unittest.TestCase):
             self.p1.set_score("A")
 
     def test_add_score(self):
-        """ Test adding a valid score and handling invalid score input. """
+        """Test adding a valid score and handling invalid score input."""
         self.p1.add_score(5)
         self.assertEqual(self.p1.score, 5)
 
@@ -32,7 +31,7 @@ class TestPlayer(unittest.TestCase):
             self.p1.add_score("A")
 
     def test_set_name(self):
-        """Test setting a valid name and handling invalid name input. """
+        """Test setting a valid name and handling invalid name input."""
         self.p1.set_name('Marcus123')
         self.assertEqual(self.p1.name, "Marcus123")
 
@@ -44,7 +43,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.p1.get_name(), "Marcus")
 
     def test_get_score(self):
-        """ Test retrieving the player's score. """
+        """Test retrieving the player's score."""
         self.assertEqual(self.p1.get_score(), 0)
 
 
