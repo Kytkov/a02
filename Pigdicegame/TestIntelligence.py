d6ac17db60896f5d8790d83749f28226b20c9180
MarcusPersson97 <marcus.persson.97@hotmail.com>
2025-10-23 11:17:27 +0200

    fixed docstrings

--- Pigdicegame/TestIntelligence.py
+++ Pigdicegame/TestIntelligence.py
@@ -5,18 +5,17 @@ from Intelligence import Intelligence
 class TestIntelligence(unittest.TestCase):
     """
     Unit tests for the Intelligence class.
-
     This test suite verifies the functionality of the Intelligence class,
     including score management, name setting, and difficulty handling.
     """
 
     def setUp(self):
-        """ Set up a new Intelligence instance before each test. """
+        """Set up a new Intelligence instance before each test."""
         
         self.cpu = Intelligence()
 
     def test_set_score(self):
-        """ Test setting a valid score and handling of invalid input types. """
+        """Test setting a valid score and handling of invalid input types."""
         
         self.cpu.set_score(1)
         self.assertEqual(self.cpu.get_score(), 1)
@@ -25,7 +24,7 @@ class TestIntelligence(unittest.TestCase):
             self.cpu.set_score("B")
 
     def test_add_score(self):
-        """ Test adding to the score and validating input types. """
+        """Test adding to the score and validating input types."""
         self.cpu.add_score(2)
         self.assertEqual(self.cpu.score, 2)
 
@@ -33,15 +32,15 @@ class TestIntelligence(unittest.TestCase):
             self.cpu.add_score("C")
 
     def test_get_score(self):
-        """ Test retrieving the current score. """
+        """Test retrieving the current score."""
         self.assertEqual(self.cpu.get_score(), 0)
 
     def test_get_name(self):
-        """ Test retrieving the name of the Intelligence instance. """
+        """Test retrieving the name of the Intelligence instance."""
         self.assertEqual(self.cpu.get_name(), "CPU")
 
     def test_set_name(self):
-        """ Test setting a valid name and handling of invalid input types. """
+        """Test setting a valid name and handling of invalid input types."""
         self.cpu.set_name("CPU123")
         self.assertEqual(self.cpu.name, "CPU123")
 
@@ -49,7 +48,7 @@ class TestIntelligence(unittest.TestCase):
             self.cpu.set_name(192)
 
     def test_set_difficulty(self):
-        """ Test handling of invalid difficulty inputs. """
+        """Test handling of invalid difficulty inputs."""
         self.assertEqual(
             self.cpu.set_difficulty("5"), "Incorrect input, Try again."
         )
